d374657b98ebf35f5c52d55bbf6d450762030db9
crystalittle <104913896+crystalittle@users.noreply.github.com>
2024-09-17 22:38:51 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,67 +1,46 @@
 <?php
-
 use App\Http\Controllers\FilmesController;
 use App\Http\Controllers\UsuariosController;
 use Illuminate\Support\Facades\Route;
 
-//filmes
 
 Route::get('/', function () {
-return view('iniciar');
-}) -> name('index');
+    return view('iniciar');
+})->name('index');
 
-Route::get('/filmes', 
-[FilmesController::class, 'index'])
-->name('filmes');
+Route::get('/filmes', [FilmesController::class, 'index'])->name('filmes');
 
-Route::get('/filmes/cadastrar',
-[FilmesController::class, 'cadastrar'])
-->name('filmes.cadastrar');
+Route::get('/filmes/cadastrar', [FilmesController::class, 'cadastrar'])->name('filmes.cadastrar');
 
-Route::post('/filmes/cadastrar',
-[FilmesController::class, 'gravar'])
-->name('filmes.gravar');
+Route::post('/filmes/cadastrar',[FilmesController::class, 'gravar'])->name('filmes.gravar');
 
-Route::get('/filmes/apagar/{filme}',
-[FilmesController::class, 'apagar'])->name('filmes.apagar');
+Route::get('/filmes/apagar/{filme}', [FilmesController::class, 'apagar'])->name('filmes.apagar');
 
-Route::delete('/filmes/apagar/{filme}',
-[FilmesController::class, 'deletar']);
+Route::delete('/filmes/apagar/{filme}', [FilmesController::class, 'deletar']);
 
-Route::get('/filmes/editar/{filme}', [FilmesController::class, 'editar'])->name('filmes.editar');
+Route::get('/filmes/editar/{filme}', [FilmesController::class, 'editar'])-> name('filmes.editar');
 
 Route::put('/filmes/editar/{filme}', [FilmesController::class, 'editarGravar']);
 
-//usuarios
+Route::prefix('usuarios')->middleware('auth')->group(function() {
+    Route::get('/', [UsuariosController::class, 'index'])->name('usuarios');
     
-Route::prefix('usuarios')->middleware('auth')->group(function(){
-    Route::get('/', [UsuariosController::class, 'index'])
-    ->name('usuarios');
+    Route::get('/apagar/{usuario}', [UsuariosController::class, 'apagar'])->name('usuarios.apagar');
 
-Route::get('/cadastrar', 
-[UsuariosController::class, 'cadastrar'])
-->name('usuarios.cadastrar');
+    Route::delete('/apagar/{usuario}', [UsuariosController::class, 'deletar']);
 
-Route::post('/gravar', 
-[UsuariosController::class, 'gravar'])
-->name('usuarios.gravar');
+    Route::get('/editar/{usuario}', [UsuariosController::class, 'editar'])-> name('usuarios.editar');
 
-Route::get('/editar/{usuario}', [UsuariosController::class, 'editar'])
-->name('usuarios.editar');
-
-Route::put('/editar/{usuario}', [UsuariosController::class, 'editarGravar'])
-->name('usuarios.editarGravar');
-
-Route::get('/apagar/{usuario}',
-[UsuariosController::class, 'apagar'])->name('usuarios.apagar');
-
-Route::delete('/apagar/{usuarios}',
-[UsuariosController::class, 'remove'])
-->name('usuarios.apagar');
+    Route::put('/editar/{usuario}', [UsuariosController::class, 'editarGravar']);
 });
 
-Route::get('login', [UsuariosController::class, 'login'])->name('login');
+Route::get('/cadastrar', [UsuariosController::class, 'create'])->name('usuarios.cadastrar');
+    
+Route::post('/cadastrar', [UsuariosController::class, 'insert'])->name('usuarios.gravar');
 
-Route::post('login', [UsuariosController::class, 'login'])->name('login');
+Route::get('/login', [UsuariosController::class, 'login'])->name('login');
+
+Route::post('/login', [UsuariosController::class, 'login']);
 
 Route::get('logout', [UsuariosController::class, 'logout'])->name('logout');
+
