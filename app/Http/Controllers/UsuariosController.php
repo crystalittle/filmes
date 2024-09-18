3932d8a8a033bdabf159d7ab0c86f440573154e8
crystalittle <104913896+crystalittle@users.noreply.github.com>
2024-09-17 22:24:06 -0300

    UsuariosController.php

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -9,85 +9,70 @@ use Illuminate\Support\Facades\Hash;
 
 class UsuariosController extends Controller
 {
-    public function index(){
-        $dados = Usuarios::all();
-        return view('usuarios.index', [
-            'usuarios' => $dados,
-        ]);
+    public function index()
+    {
+        $usuarios = Usuarios::orderBy('name', 'asc')->get();
+
+        return view('usuarios.index', ['usuarios' => $usuarios, 'pagina' => 'usuarios']);
     }
- 
-    public function cadastrar(){
+
+    public function create()
+    {
         return view('usuarios.cadastrar');
     }
 
-    public function gravar(Request $form){ //quando acessar animais cadastrar via post, submentendo o form
+    public function insert(Request $form)
+    {
         $dados = $form->validate([
-            'nome' => 'required|min:3',
-            'email' => 'required|',
-            'username' => 'required',
-            'password' => 'required',
-            'admin' => 'boolean'
-        ]);    
+            'name' => 'required',
+            'email' => 'email|required|unique:usuarios',
+            'username' => 'required|min:3',
+            'password' => 'required|min:3',
+            'admin' => 'boolean',
+        ]);
 
         $dados['password'] = Hash::make($dados['password']);
 
         Usuarios::create($dados);
-        
+
         return redirect()->route('usuarios');
-   }
+    }
 
-   public function login(Request $form){
-       if($form->isMethod('POST')){
-        $credenciais = $form->validate([
-            'username' => 'required',
-            'password' => 'required',
-        ]);
+    public function login(Request $form) {
+        if ($form->isMethod('POST')) {
 
-        if(Auth::attempt($credenciais)){
-            return redirect()->intended(route('index'));
-        }else{
-            return redirect()->route('login')
-            ->with('erro', 'Usuário ou senha inválidos');
+            // Pega os dados do formulário
+            $credenciais = $form->validate([
+                'username' => 'required',
+                'password' => 'required',
+
+
+            ]);
+
+            // Tenta fazer o login
+            if (Auth::attempt($credenciais)) {
+                return redirect()->intended(route('index'));
+            } else {
+                return redirect()->route('login')->with('erro', 'Usuário ou senha inválidos');
+            }
         }
 
-       }
         return view('usuarios.login');
-   }
+    }
 
-   public function logout(){
-    Auth::logout();
-    return redirect()->route('index');
-   }
-
-    public function apagar(Usuarios $usuario){
+    public function logout() {
+        Auth::logout();
+        return redirect()->route('index');
+    }
+    public function apagar(usuarios $usuario) {
         return view('usuarios.apagar', [
             'usuario' => $usuario,
         ]);
     }
 
-
-   public function editar(usuarios $usuario) {
-    return view('usuarios/editar', ['usuario' => $usuario]);
-   }
-
-   public function editarGravar(Request $form, Usuarios $usuario)
-   {
-    $dados = $form->validate([
-    'nome' => 'required|max:255',
-    'email' => 'required|max:225',
-    'username' => '',
-    'password' => '',
-    ]);
-
-    $usuario->fill($dados);
-    $usuario->save();
-    return redirect() -> route('usuarios');
-
-
-    }    
-    public function deletar(Usuarios $usuario){
-        $usuario -> delete();
-        return redirect() -> route('usuarios');
-     }
-     
+    public function deletar(usuarios $usuario) {
+        $usuario->delete();
+        return redirect()->route('usuarios');
+    }
 }
+
